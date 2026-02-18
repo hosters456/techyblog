30e53eb2010004903721cdac065c48503f42af06
Coder Shivam <codershivam@proton.me>
2026-02-18 14:00:11 +0530

    Index Page Changes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center md:flex-row md:items-start md:justify-start">
     <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
         Coming Soon 🚀
     </h1>
