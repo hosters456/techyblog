2637d850c4b47e96ab69c4bc8d7c2391241ea7f4
Coder Shivam <codershivam@proton.me>
2026-02-20 10:01:07 +0530

    main

--- app/layout.js
+++ app/layout.js
@@ -66,6 +66,8 @@ export const metadata = {
   },
 };
 
+import Script from 'next/script';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -86,6 +88,22 @@ export default function RootLayout({ children }) {
             </div>
           </AuthProvider>
         </ThemeProvider>
+
+        {/* Ad Network Script */}
+        <Script id="ad-network-script" strategy="afterInteractive">
+          {`
+            (function(cjw){
+            var d = document,
+                s = d.createElement('script'),
+                l = d.scripts[d.scripts.length - 1];
+            s.settings = cjw || {};
+            s.src = "\\/\\/elderlygoal.com\\/c\\/DZ9Y6Pb.2s5\\/lmSdWgQb9oN\\/jJgAxFNRD\\/Qm4vNISM0A2ROnDgEl0_NHDCg-5K";
+            s.async = true;
+            s.referrerPolicy = 'no-referrer-when-downgrade';
+            l.parentNode.insertBefore(s, l);
+            })({})
+          `}
+        </Script>
       </body>
     </html>
   );
